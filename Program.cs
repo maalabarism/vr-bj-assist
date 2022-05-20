d8dc8a39ad2b9e47c0cce690e58104e7d45376a4
Stefano Signorelli <stefanoe.signorelli@gmail.com>
2022-05-20 15:41:51 -0700

    First commit.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World welcome to c#!");
+        }
+    }
+}
